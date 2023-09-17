7e90b18a8e39e7de5ee51da97021eebd8fe7023e
marakawa <marakawa@marakawa.github.io>
2023-09-17 10:33:24 +0900

    Add snippets.html

---
+++ src/snippets.ts
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+    const inputElem = document.querySelector('#input') as HTMLInputElement
+    const outputElem = document.querySelector('#output') as HTMLInputElement
+    inputElem.addEventListener('keyup', (e) => {
+        const inputText = inputElem.value.trim()
+        const outputJson = {
+            scope: 'javascript,typescript,typescriptreact',
+            prefix: '_',
+            body: inputText.split('\n').map((line) => line.replace('"', '\\"')),
+        }
+        outputElem.value = JSON.stringify(outputJson, undefined, 4)
+    })
+})
